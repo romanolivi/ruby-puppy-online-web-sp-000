4dea6d9676a91a590c7a1834898e10bd37b7f5bd
Romano Livi <romano_livi@outlook.com>
2020-12-18 18:11:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,17 +5,25 @@ class Dog
   
   def initialize(name)
     @name = name
-    @@all << self 
+    self.save
   end
   
   def self.all
     @@all 
   end
   
+  def save
+    @@all << self
+  end
+  
   def self.print_all
     @@all.each do |dog|
       puts dog.name
     end
+  end
+  
+  def self.clear_all
+    @@all.clear 
   end
   
 end
